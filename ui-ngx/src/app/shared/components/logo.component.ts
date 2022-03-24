1006efcc3dbdedeb52a7967630113dd7dc8a11d9
Rico Pahlisch <rico.pahlisch@kiwigrid.com>
2022-03-24 17:09:16 +0100

    try build

--- ui-ngx/src/app/shared/components/logo.component.ts
+++ ui-ngx/src/app/shared/components/logo.component.ts
@@ -23,7 +23,7 @@ import { Component } from '@angular/core';
 })
 export class LogoComponent {
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/grayc_logo.png';
 
   gotoThingsboard(): void {
     window.open('https://thingsboard.io', '_blank');
